1f30c3a189ba3e6cc3c09ef73138e7071db2db36
emre candan <wastorist@gmail.com>
2021-10-10 13:05:14 +0300

    Odev4 gerceklendi.

---
+++ Odev4.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+fs.writeFileSync('employees.json', ' {"name": "Employee 1 Name", "salary": 2000}', 'utf8', (err) => {
+    if (err) console.log(err);
+});
+
+
+fs.readFile('employees.json', 'utf8', (err, data) => {
+    if (err) console.log(err);                        
+    console.log(data);                                 
+  })
+
+fs.appendFileSync('employees.json', '{"name": "Employee 2 Name", "salary": 7000}', 'utf8', (err) => {
+    if (err) console.log(err);
+});
+
+fs.unlink('employees.json', (err) => {
+    if (err) console.log(err);
+});
